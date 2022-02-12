a2f44d5f250c7132b08a31bdd7f9b8089b88d47b
rajkumar060301 <raj.kumar_cs19@gla.ac.in>
2022-02-13 01:59:40 +0530

    Welcome to java

---
+++ src/introduction/WelComeToJava.java
@@ -0,0 +1,14 @@
+package introduction;/*
+ * Author Name: Raj Kumar
+ * IDE: IntelliJ IDEA Ultimate Edition
+ * JDK: 17 version
+ * Date: 13-Feb-22
+ */
+
+public class WelComeToJava {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
