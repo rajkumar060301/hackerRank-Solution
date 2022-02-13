fc504c1b943be6f4b2cf199728ee0f64f685c307
rajkumar060301 <raj.kumar_cs19@gla.ac.in>
2022-02-13 19:12:46 +0530

    loop I question

---
+++ src/introduction/LoopFirst.java
@@ -0,0 +1,18 @@
+package introduction;/*
+ * Author Name: Raj Kumar
+ * IDE: IntelliJ IDEA Ultimate Edition
+ * JDK: 17 version
+ * Date: 13-Feb-22
+ */
+
+import java.util.Scanner;
+
+public class LoopFirst {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " " + " = " + i * n);
+        }
+    }
+}
